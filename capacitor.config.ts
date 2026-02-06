aa55328b0f1c78c1ca0ccbb9b86b73be150fcc3a
burnout909 <znlwmdhkd1@yonsei.ac.kr>
2026-02-05 17:09:59 -0800

    add ios feature

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,15 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://seniorbuddy.vercel.app',
     cleartext: false
-  }
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: process.env.NEXT_PUBLIC_GOOGLE_WEB_CLIENT_ID || '',
+      iosClientId: process.env.NEXT_PUBLIC_GOOGLE_IOS_CLIENT_ID || '',
+      forceCodeForRefreshToken: false,
+    },
+  },
 };
 
 export default config;
